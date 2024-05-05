d9e28260a76bf4f15885ea1f24c2a22e1fcf5557
Irene <terna.legend@gmail.com>
2024-05-05 15:47:35 +0200

    created start page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same.
+          Click each die to freeze it at its current value between rolls.</p>
+        <button className='start-button'>Start Game</button>
+      </main>
+    </div>
+  );
+}
+
+export default App;
