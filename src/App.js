aabcbd2b0694b7b5d3c13fb33e9a3cc719547aea
Irene <terna.legend@gmail.com>
2024-05-06 12:31:51 +0200

    style start screen + add to-main screen switch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import React from "react"
 import './App.css';
+import TimerIcon from '@mui/icons-material/Timer';
 
 function App() {
+
+  const [screen, setScreen] = React.useState("start")
+
+  function handleScreenChange() {
+    setScreen("main")
+  }
+
   return (
     <div className="App">
       <main>
-        <h1 className="title">Tenzies</h1>
-        <p className="instructions">Roll until all dice are the same.
-          Click each die to freeze it at its current value between rolls.</p>
-        <button className='start-button'>Start Game</button>
+        {screen === "start" ? (
+          <>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same.
+              Click each die to freeze it at its current value between rolls.</p>
+            <button className='start-button' onClick={handleScreenChange}>Start Game
+              <TimerIcon className='timer-icon' />
+            </button>
+          </>) : (
+          <div>
+            <h1>Main Screen</h1>
+          </div>
+        )}
       </main>
     </div>
   );
