f49837c2ef6f0a677af5e8063b0aa58ce8615162
Irene <terna.legend@gmail.com>
2024-05-09 16:48:52 +0200

    full tenzies game functionality

--- src/App.js
+++ src/App.js
@@ -4,28 +4,51 @@ import TimerIcon from '@mui/icons-material/Timer';
 import { nanoid } from 'nanoid'
 import Die from "./Die";
 import picData from "./data"
-
+import Fireworks from "react-canvas-confetti/dist/presets/fireworks";
 
 function App() {
-
-
   const [screen, setScreen] = React.useState("start")
   const [dice, setDice] = React.useState(allNewDice())
+  const [count, setCount] = React.useState(0)
+  const [tenzies, setTenzies] = React.useState(false)
+  const [time, setTime] = React.useState(0)
+  const intervalRef = React.useRef(null)
 
   function handleScreenChange() {
     setScreen("main")
+    setCount(0)
   }
+
+  function startTimer() {
+    const id = setInterval(() => { setTime(oldTime => oldTime + 1) }, 1000)
+    intervalRef.current = id;
+  }
+
+
+  function createSeparateDie() {
+    const randomPictureObj = picData[Math.floor(Math.random() * 6)]
+    return {
+      number: randomPictureObj.number,
+      defaultValue: randomPictureObj.imgDefault,
+      heldValue: randomPictureObj.imgHeld,
+      id: nanoid(),
+      isHeld: false
+    }
+  }
+
+  React.useEffect(() => {
+    const referenceNumber = dice[0].number
+    if (dice.every(die => die.isHeld) && dice.every(die => die.number === referenceNumber)) {
+      setTenzies(true);
+      clearInterval(intervalRef.current)
+    }
+  }, [dice])
+
 
   function allNewDice() {
     const newDice = []
     for (let i = 0; i < 10; i++) {
-      const randomPictureObj = picData[Math.floor(Math.random() * 6)]
-      newDice.push({
-        defaultValue: randomPictureObj.imgDefault,
-        heldValue: randomPictureObj.imgHeld,
-        id: nanoid(),
-        isHeld: false
-      })
+      newDice.push(createSeparateDie())
     }
     return newDice
   }
@@ -40,6 +63,22 @@ function App() {
     }))
   }
 
+  function rollDice() {
+    if (!tenzies) {
+      setDice(prevDice => prevDice.map(die => {
+        return die.isHeld ? die : createSeparateDie()
+      }))
+    } else {
+      setTenzies(false)
+      setDice(allNewDice())
+      setScreen("start")
+    }
+  }
+
+  function addCount() {
+    setCount(prevCount => prevCount + 1)
+  }
+
   const diceElements = dice.map(die => <Die key={die.id}
     defaultValue={die.defaultValue}
     id={die.id}
@@ -50,22 +89,24 @@ function App() {
 
   return (
     <div className="App">
+      {tenzies && <Fireworks autorun={{ speed: 3 }} />}
       <main>
         {screen === "start" ? (
           <div className="wrapper">
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same.
               Click each die to freeze it at its current value between rolls.</p>
-            <button className='start-button' onClick={handleScreenChange}>Start Game
+            <button className='start-button' onClick={() => { handleScreenChange(); startTimer() }}>Start Game
               <TimerIcon className='timer-icon' />
             </button>
           </div>) : (
           <div className="tenzies-screen">
-            <h2 className="attemts-counter">Number of attempts: 0</h2>
+            <div className="timer">Time:{time}sec</div>
+            <h2 className="attemts-counter">Number of attempts: {count}</h2>
             <div className="dice-container">
               {diceElements}
             </div>
-            <button className='roll-button'>Roll</button>
+            <button className='roll-button' onClick={() => { rollDice(); addCount() }}>{tenzies ? "New Game" : "Roll"}</button>
           </div>
         )}
       </main>
